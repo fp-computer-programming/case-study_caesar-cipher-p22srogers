fa456a53464e3423768b1e234beae3addd1404b7
P22srogers <p22srogers@fairfieldprep.org>
2022-04-28 22:12:48 -0400

    done

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,8 +1,7 @@
+# Author: SMR (AMDG) 04/29/22
 
-
-# Imports
+# Initial Import
 from string import ascii_uppercase
-
 
 # Functions
 def cipher_key(shift):
@@ -10,19 +9,52 @@ def cipher_key(shift):
     shifted_letters = ascii_uppercase[int(shift):] + ascii_uppercase[:int(shift)]
 
     return dict(zip(original_letters, shifted_letters))
-
-
-def shift_line(line, dict_key):
+def shifting_line(line, dict_key):
+    # Blank space for new lines
     new_line = ""
-    # Add code here
+    
+    # Have to create a for loop here in order to translate correctly for every character
+    for character in line: 
+        if character == " ":
+            new_line += " "
+            continue
+       
+        elif character == "\n":
+         
+         # Need a new line in order to keep the indents in place 
+            new_line += "\n" 
+            continue
+       
+       #Then need another elif statement to  keep punctuation in check for later
+        elif character == "!" or character == "," or character == "'": 
+            new_line += character
+            continue
+        
+        # Convert each character (letter) to uppercase
+        character = character.upper() 
+       
+       # Then the blankspace variable from above gets filled in with new information
+        new_line = new_line + dict_key[character] 
     return new_line
 
-
 def encrypt_message(filename, dict_key):
-    # Add code here
+# creating blank space for list    
+    lst = [] 
+   # creating blank space for final statement
+    last_statement = "" 
+    it = open(filename)
+    with it as file:
+        for l in file: 
+        # adds the list to the line that shifted    
+            lst += shifting_line(l,dict_key)
+        for lines in lst: 
+            end += lines
+        file = open("new_file.txt","w")
+   # Final Write statement to store in new file     
+        file.write(last_statement) 
+        file.close()
 
-
-# Main
+# Input statements prompting user to enter the file as well as shift value. Once this occurs, stores information as variable.
 user_file = input("Please enter a file to be encrypted: ")
 shift_value = input("Please enter a shift value: ")
 
